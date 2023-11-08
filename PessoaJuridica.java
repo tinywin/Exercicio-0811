6682352d9e072d46822ada18834eff5394309b4b
Laura Barbosa Henrique <laurahenrique9@gmail.com>
2023-11-08 17:23:59 -0300

    Add files via upload

---
+++ PessoaJuridica.java
@@ -0,0 +1,17 @@
+public class PessoaJuridica extends Pessoa {
+  private String cnpj;
+
+  public PessoaJuridica(String nome, String endereco, String cnpj) {
+  super(nome, endereco);
+    this.cnpj = cnpj;
+  }
+
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
+  
+}
